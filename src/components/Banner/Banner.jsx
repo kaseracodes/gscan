542a632b860a2a35b54dc66240845d67b6bae1f8
kaseracodes <shivamkasera0951@gmail.com>
2024-10-02 15:59:51 +0530

    added downlaod brochure link in home page banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,17 @@
 /* eslint-disable react/prop-types */
+import { COLORS } from "../../assets/constants";
 import styles from "./Banner.module.css";
+import Button from "../button/Button";
 
 const Banner = ({ imagePath, heading, description }) => {
+
+  const downloadBrochure = () => {
+    const link = document.createElement('a');
+    link.href = 'https://drive.google.com/uc?export=download&id=1Baf-KGYF6WpOwwWuDwPpRG9pQ7aBfcyD'; // Replace with the actual path to your PDF
+    link.download = 'Brochure.pdf'; // Set the desired filename for download
+    link.click();
+  };
+
   return (
     <div
       style={{ backgroundImage: `url("${imagePath}")` }}
@@ -14,6 +24,11 @@ const Banner = ({ imagePath, heading, description }) => {
       <p
         dangerouslySetInnerHTML={{ __html: description }}
         className={styles.desc}
+      />
+      <Button
+        content="Brochure"
+        bgColor={COLORS.orange}
+        onClick={downloadBrochure}
       />
     </div>
   );

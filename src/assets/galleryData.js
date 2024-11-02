15cf0ade5d3e6acffae6b022f074135749903e0b
kaseracodes <shivamkasera0951@gmail.com>
2024-11-03 01:47:37 +0530

    modified gallery page

--- src/assets/galleryData.js
+++ src/assets/galleryData.js
@@ -2,165 +2,164 @@ export const GalleryData = [
   {
     id: 0,
     imagePath: [
-      "/images/gallery/image1.png",
-      "/images/gallery/image2.png",
-      "/images/gallery/image3.png",
+      "/images/gallery/firstpost2.jpeg",
+      "/images/gallery/firstpost3.jpeg",
     ],
-    logoPath: "/images/gallery/logo1.png",
+    logoPath: "/images/gallery/infrastructure.png",
     category: "Infrastructure",
     date: "4th November, 2023",
-    heading: "Taking the big leaps!",
+    heading: "Precision & Durability in Every Sliver Can",
     description:
-      "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
+      "Our sliver cans are built for durability, smooth handling, and precision. Made from premium materials, they reduce fiber damage, boost efficiency, and deliver consistent, reliable performance in every use.",
   },
 
-  {
-    id: 1,
-    imagePath: [
-      "/images/gallery/image1.png",
-      "/images/gallery/image2.png",
-      "/images/gallery/image3.png",
-    ],
-    logoPath: "/images/gallery/logo1.png",
-    category: "Infrastructure",
-    date: "4th November, 2023",
-    heading: "Taking the big leaps!",
-    description:
-      "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
-  },
+  // {
+  //   id: 1,
+  //   imagePath: [
+  //     "/images/gallery/image1.png",
+  //     "/images/gallery/image2.png",
+  //     "/images/gallery/image3.png",
+  //   ],
+  //   logoPath: "/images/gallery/logo1.png",
+  //   category: "Infrastructure",
+  //   date: "4th November, 2023",
+  //   heading: "Taking the big leaps!",
+  //   description:
+  //     "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
+  // },
 
-  {
-    id: 2,
-    imagePath: [
-      "/images/gallery/image1.png",
-      "/images/gallery/image2.png",
-      "/images/gallery/image3.png",
-    ],
-    logoPath: "/images/gallery/logo1.png",
-    category: "Infrastructure",
-    date: "4th November, 2023",
-    heading: "Taking the big leaps!",
-    description:
-      "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
-  },
+  // {
+  //   id: 2,
+  //   imagePath: [
+  //     "/images/gallery/image1.png",
+  //     "/images/gallery/image2.png",
+  //     "/images/gallery/image3.png",
+  //   ],
+  //   logoPath: "/images/gallery/logo1.png",
+  //   category: "Infrastructure",
+  //   date: "4th November, 2023",
+  //   heading: "Taking the big leaps!",
+  //   description:
+  //     "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
+  // },
 
-  {
-    id: 3,
-    imagePath: [
-      "/images/gallery/image1.png",
-      "/images/gallery/image2.png",
-      "/images/gallery/image3.png",
-    ],
-    logoPath: "/images/gallery/logo1.png",
-    category: "Infrastructure",
-    date: "4th November, 2023",
-    heading: "Taking the big leaps!",
-    description:
-      "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
-  },
+  // {
+  //   id: 3,
+  //   imagePath: [
+  //     "/images/gallery/image1.png",
+  //     "/images/gallery/image2.png",
+  //     "/images/gallery/image3.png",
+  //   ],
+  //   logoPath: "/images/gallery/logo1.png",
+  //   category: "Infrastructure",
+  //   date: "4th November, 2023",
+  //   heading: "Taking the big leaps!",
+  //   description:
+  //     "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
+  // },
 
-  {
-    id: 4,
-    imagePath: [
-      "/images/gallery/image1.png",
-      "/images/gallery/image2.png",
-      "/images/gallery/image3.png",
-    ],
-    logoPath: "/images/gallery/logo1.png",
-    category: "Infrastructure",
-    date: "4th November, 2023",
-    heading: "Taking the big leaps!",
-    description:
-      "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
-  },
+  // {
+  //   id: 4,
+  //   imagePath: [
+  //     "/images/gallery/image1.png",
+  //     "/images/gallery/image2.png",
+  //     "/images/gallery/image3.png",
+  //   ],
+  //   logoPath: "/images/gallery/logo1.png",
+  //   category: "Infrastructure",
+  //   date: "4th November, 2023",
+  //   heading: "Taking the big leaps!",
+  //   description:
+  //     "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
+  // },
 
-  {
-    id: 5,
-    imagePath: [
-      "/images/gallery/image1.png",
-      "/images/gallery/image2.png",
-      "/images/gallery/image3.png",
-    ],
-    logoPath: "/images/gallery/logo1.png",
-    category: "Infrastructure",
-    date: "4th November, 2023",
-    heading: "Taking the big leaps!",
-    description:
-      "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
-  },
+  // {
+  //   id: 5,
+  //   imagePath: [
+  //     "/images/gallery/image1.png",
+  //     "/images/gallery/image2.png",
+  //     "/images/gallery/image3.png",
+  //   ],
+  //   logoPath: "/images/gallery/logo1.png",
+  //   category: "Infrastructure",
+  //   date: "4th November, 2023",
+  //   heading: "Taking the big leaps!",
+  //   description:
+  //     "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
+  // },
 
-  {
-    id: 6,
-    imagePath: [
-      "/images/gallery/image1.png",
-      "/images/gallery/image2.png",
-      "/images/gallery/image3.png",
-    ],
-    logoPath: "/images/gallery/logo1.png",
-    category: "Infrastructure",
-    date: "4th November, 2023",
-    heading: "Taking the big leaps!",
-    description:
-      "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
-  },
+  // {
+  //   id: 6,
+  //   imagePath: [
+  //     "/images/gallery/image1.png",
+  //     "/images/gallery/image2.png",
+  //     "/images/gallery/image3.png",
+  //   ],
+  //   logoPath: "/images/gallery/logo1.png",
+  //   category: "Infrastructure",
+  //   date: "4th November, 2023",
+  //   heading: "Taking the big leaps!",
+  //   description:
+  //     "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
+  // },
 
-  {
-    id: 7,
-    imagePath: [
-      "/images/gallery/image1.png",
-      "/images/gallery/image2.png",
-      "/images/gallery/image3.png",
-    ],
-    logoPath: "/images/gallery/logo1.png",
-    category: "Infrastructure",
-    date: "4th November, 2023",
-    heading: "Taking the big leaps!",
-    description:
-      "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
-  },
+  // {
+  //   id: 7,
+  //   imagePath: [
+  //     "/images/gallery/image1.png",
+  //     "/images/gallery/image2.png",
+  //     "/images/gallery/image3.png",
+  //   ],
+  //   logoPath: "/images/gallery/logo1.png",
+  //   category: "Infrastructure",
+  //   date: "4th November, 2023",
+  //   heading: "Taking the big leaps!",
+  //   description:
+  //     "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
+  // },
 
-  {
-    id: 8,
-    imagePath: [
-      "/images/gallery/image1.png",
-      "/images/gallery/image2.png",
-      "/images/gallery/image3.png",
-    ],
-    logoPath: "/images/gallery/logo1.png",
-    category: "Infrastructure",
-    date: "4th November, 2023",
-    heading: "Taking the big leaps!",
-    description:
-      "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
-  },
+  // {
+  //   id: 8,
+  //   imagePath: [
+  //     "/images/gallery/image1.png",
+  //     "/images/gallery/image2.png",
+  //     "/images/gallery/image3.png",
+  //   ],
+  //   logoPath: "/images/gallery/logo1.png",
+  //   category: "Infrastructure",
+  //   date: "4th November, 2023",
+  //   heading: "Taking the big leaps!",
+  //   description:
+  //     "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
+  // },
 
-  {
-    id: 9,
-    imagePath: [
-      "/images/gallery/image1.png",
-      "/images/gallery/image2.png",
-      "/images/gallery/image3.png",
-    ],
-    logoPath: "/images/gallery/logo1.png",
-    category: "Infrastructure",
-    date: "4th November, 2023",
-    heading: "Taking the big leaps!",
-    description:
-      "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
-  },
+  // {
+  //   id: 9,
+  //   imagePath: [
+  //     "/images/gallery/image1.png",
+  //     "/images/gallery/image2.png",
+  //     "/images/gallery/image3.png",
+  //   ],
+  //   logoPath: "/images/gallery/logo1.png",
+  //   category: "Infrastructure",
+  //   date: "4th November, 2023",
+  //   heading: "Taking the big leaps!",
+  //   description:
+  //     "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
+  // },
 
-  {
-    id: 10,
-    imagePath: [
-      "/images/gallery/image1.png",
-      "/images/gallery/image2.png",
-      "/images/gallery/image3.png",
-    ],
-    logoPath: "/images/gallery/logo1.png",
-    category: "Infrastructure",
-    date: "4th November, 2023",
-    heading: "Taking the big leaps!",
-    description:
-      "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
-  },
+  // {
+  //   id: 10,
+  //   imagePath: [
+  //     "/images/gallery/image1.png",
+  //     "/images/gallery/image2.png",
+  //     "/images/gallery/image3.png",
+  //   ],
+  //   logoPath: "/images/gallery/logo1.png",
+  //   category: "Infrastructure",
+  //   date: "4th November, 2023",
+  //   heading: "Taking the big leaps!",
+  //   description:
+  //     "Whether you want to meet face-to-face with fantastic new suppliers or buyers, promote your latest product to thousands of leading industry professionals or learn about the very latest local and global innovations and trends, look no further than Fi Global. Food ingredients Global is the unrivalled world leader at providing food and beverage professionals with the personalised business solutions they need.",
+  // },
 ];
